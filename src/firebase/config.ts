536a8fba67291cbd393c92372f9d1958a51c7c0f
Ugesh Simkhada <ugeshsimkhada@gmail.com>
2026-03-23 09:54:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2608026175-eeb50",
+  "appId": "1:996064252531:web:39df449190ac591a431006",
+  "apiKey": "AIzaSyBzer8IQ4EuehCfkuA3guK45t6eJVFd2Mc",
+  "authDomain": "studio-2608026175-eeb50.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "996064252531"
+};
